b1aa4ccca2ec50af280dfb6a3f7417f257d639bd
Dmitri Urbanovich <1124004@gmail.com>
2021-02-17 22:10:41 +0300

    add text and title to js

--- index.js
+++ index.js
@@ -6,3 +6,55 @@ const sections = [
     { type: 'text', value: loremText },
     { type: 'columns', value: [loremText, loremText, loremText] }
 ];
+
+const $content = document.querySelector('#cv');  // $ был в jquery но можно не использовать
+
+// sections.forEach( section => {
+//     let html = '<h2>Hello from JS</h2>';
+//     $content.insertAdjacentHTML('beforeend', html) // вставляется динамический контент 
+// })
+
+sections.forEach( section => {
+    let html = '';
+
+    if (section.type === 'title') {
+        html = title(section);
+
+    } else if (section.type === 'text') {
+        html = text(section);
+
+    } else if (section.type === 'columns') {
+        html = columns(section);
+    };
+
+    $content.insertAdjacentHTML('beforeend', html);
+})
+
+function title(section) {
+    return `
+    <section class="row">
+        <div class="col-sm">
+          <h2>${section.value}</h2>
+        </div>
+      </section>`
+}
+
+function text(section) {
+    return `
+    <section class="row">
+    <div class="col-sm">
+      <p>${section.value}</p>
+    </div>
+  </section>`
+}
+
+function columns(section) {
+    const html = section.value.map(
+        item => {
+            return `
+            <div class="col-sm">
+                <p>${section.value}</p>
+            </div>`
+        }
+    )
+}
