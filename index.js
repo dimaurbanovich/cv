39dea3ac3722c1aab52001eb5f39c360088d3c46
Dmitri Urbanovich <1124004@gmail.com>
2021-02-17 21:37:39 +0300

    sections model

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const loremText = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta vitae beatae nesciunt. Alias, officia consequuntur quia porro inventore aspernatur facilis vitae earum ratione nobis voluptate, sunt voluptas fugiat perferendis suscipit.';
+
+const sections = [
+    { type: 'title', value: 'Hello world!' }, // объект без названия
+    { type: 'text', value: loremText },
+    { type: 'columns', value: [loremText, loremText, loremText] }
+];
