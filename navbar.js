0e97bc407a838e21c3dad24b0064770049c001d4
Dmitri Urbanovich <1124004@gmail.com>
2021-02-19 00:02:22 +0300

    add links to navbar

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-const navigation = [
+const links = [
     { type: 'home', value: 'Home' },
     { type: 'about', value: 'About' },
     { type: 'resume', value: 'Resume' },
@@ -7,16 +7,44 @@ const navigation = [
 
 const $nav = document.querySelector('#navbar')
 
-navigation.forEach( nav => {
+
+let newlinks = links.forEach( link => {
     let html = '';
 
-    if ( nav.type === 'home' ) {
+    if ( link.type === 'home' ) {    
+        html = home(link);
 
-    } else if ( nav.type === 'about' ) {
+    } else if ( link.type === 'about' ) {
+        html = about(link);
 
-    } else if ( nav.type === 'resume' ) {
+    } else if ( link.type === 'resume' ) {
+        html = resume(link);
 
-    } else if ( nav.type === 'contacts' ) {
+    } else if ( link.type === 'contacts' ) {
+        html = contacts(link);
 
     };
+
+    $nav.insertAdjacentHTML('beforeend', '<ul>');
+    $nav.insertAdjacentHTML('beforeend', html);
+    $nav.insertAdjacentHTML('beforeend', '</ul>');
 });
+
+
+
+
+function home(link) {
+    return `<li><a href="#home" class="nav-link scrollto"><i class="bi bi-house-door"></i> <span>${link.value}</span></a></li>`;
+};
+
+function about(link) {
+    return `<li><a href="#about" class="nav-link scrollto"><i class="bi bi-person"></i> <span>${link.value}</span></a></li>`;
+};
+
+function resume(link) {
+    return `<li><a href="#resume" class="nav-link scrollto"><i class="bi bi-file-earmark-person"></i> <span>${link.value}</span></a></li>`;
+};
+
+function contacts(link) {
+    return `<li><a href="#contacts" class="nav-link scrollto"><i class="bi bi-person-lines-fill"></i> <span>${link.value}</span></a></li>`;
+};
