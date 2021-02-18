3aabb6bfa41581ec67a6409686cf7feafea7de24
Dmitri Urbanovich <1124004@gmail.com>
2021-02-18 21:39:26 +0300

    add arr nav in navbar.js

---
+++ navbar.js
@@ -0,0 +1,22 @@
+const navigation = [
+    { type: 'home', value: 'Home' },
+    { type: 'about', value: 'About' },
+    { type: 'resume', value: 'Resume' },
+    { type: 'contacts', value: 'Contacts' }
+];
+
+const $nav = document.querySelector('#navbar')
+
+navigation.forEach( nav => {
+    let html = '';
+
+    if ( nav.type === 'home' ) {
+
+    } else if ( nav.type === 'about' ) {
+
+    } else if ( nav.type === 'resume' ) {
+
+    } else if ( nav.type === 'contacts' ) {
+
+    };
+});
